9ecf4d8b637b826f67056d78abbe110fae8c78b3
Anshul Joshi <anshuljoshi26@gmail.com>
2024-06-19 23:56:48 +0530

    New Changes

---
+++ LogicalProgamming/src/ScenarioBased/AlternateCapital.java
@@ -0,0 +1,18 @@
+package ScenarioBased;
+
+public class AlternateCapital {
+
+	
+	public static void main(String[] args) {
+		String str = "This is my bucket";
+		String[] arrStr = str.split(" ");
+		
+		for(int i = 0 ; i<arrStr.length;i++) {
+			if(i%2 == 0) {
+				arrStr[i] = arrStr[i].toUpperCase();
+			}
+		}
+		
+		System.out.println(String.join(" ", arrStr));
+	}
+}
